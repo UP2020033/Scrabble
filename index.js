69299a94e213780682992731eb7e645da7df7ff9
Joe McNeil <joe.mcneil1996@hotmail.co.uk>
2021-03-22 19:11:45 +0000

    Design change and JS input
    Design changes and the introduction of Javascript to the game

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const navBar = document.createElement('div');
+function addOuterFlexBox() {
+  const newFlexbox = document.createElement('div');
+  newFlexbox.className = 'outerFbox';
+  return newFlexbox;
+}
 
-console.log(test)
+function addGrid() {
+  const addNewGrid = document.createElement('div');
+  addNewGrid.className = 'gridContainer';
+  return addNewGrid;
+}
+
+const mainFlexBox = document.querySelector('.mainContainer').appendChild(addOuterFlexBox());
+const mainGrid = document.querySelector('.outerFbox').appendChild(addGrid());
